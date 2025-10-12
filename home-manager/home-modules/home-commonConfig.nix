eca6b8ad01ed0e5674d20f008a20181f2b005c69
fortydeux <ogtpowell@gmail.com>
2025-10-12 08:18:09 -0400

    Updates flake inputs: home-manager, hyprland, nixvim, stylix; Removes ctranslate2 override in home-commonConfig.nix due to no longer needed for build.

--- home-manager/home-modules/home-commonConfig.nix
+++ home-manager/home-modules/home-commonConfig.nix
@@ -27,15 +27,14 @@
   nixpkgs.config.allowUnfree = true;
   
   # Override packages to fix CMake compatibility issues
-  nixpkgs.overlays = [
-    (final: prev: {
-      ctranslate2 = prev.ctranslate2.overrideAttrs (oldAttrs: {
-        nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ final.cmake ];
-        cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [ "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" ];
-      });
-    })
-    
-  ];
+  # nixpkgs.overlays = [
+  #   (final: prev: {
+  #     ctranslate2 = prev.ctranslate2.overrideAttrs (oldAttrs: {
+  #       nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ final.cmake ];
+  #       cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [ "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" ];
+  #     });
+  #   })    
+  # ];
 
   home.packages = (with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
