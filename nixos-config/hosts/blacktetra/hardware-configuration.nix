d431d54f8d6a2f118cb9a684d213c71e347634d4
fortydeux <ogtpowell@gmail.com>
2026-01-21 14:01:14 -0500

    Updates hardware config for Blacktetra after reinstall.

--- nixos-config/hosts/blacktetra/hardware-configuration.nix
+++ nixos-config/hosts/blacktetra/hardware-configuration.nix
@@ -14,27 +14,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/f23db060-f4d9-4b5b-a735-1b055465b5f8";
+    { device = "/dev/disk/by-uuid/4a7ca28b-08fc-47bb-845c-c30507787a70";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/4EDF-4E7B";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/d132b0c0-b819-4112-b937-9ae62f374441"; }
     ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
